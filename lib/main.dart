50be4f1e0b5c1f7733540f946c4f37e73cd2bf72
akarshrajap <akarshrajap@gmail.com>
2026-07-25 09:45:09 +0530

    2nd commit

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyPortfolioApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Portfolio App',
+      title: 'Portfolio App2',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         useMaterial3: true,
